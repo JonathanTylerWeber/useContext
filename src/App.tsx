3c7f45c149b8e7230e44fe359f232191feade1eb
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-12-03 15:15:14 -0600

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button } from '@/components/ui/button'
 import Layer from './components/Layer'
+import { CountProvider } from './contexts/CountContext'
 
 function App() {
   return (
     <div className='flex min-h-svh flex-col items-center justify-center'>
-      <Layer>
-        <Button>Click me</Button>
-      </Layer>
+      <CountProvider>
+        <Layer>
+          <Button>Click me</Button>
+        </Layer>
+      </CountProvider>
     </div>
   )
 }
