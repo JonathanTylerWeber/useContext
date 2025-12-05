e76d86311fd9586f1ef2df890d5f7ce98b5afdd9
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-12-04 22:31:32 -0600

    add padding

--- src/components/Layer.tsx
+++ src/components/Layer.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 
 export default function Layer({ children }: { children: ReactNode }) {
   return (
-    <div className='bg-amber-400'>
+    <div className='bg-amber-400 p-6'>
       <p>This is a layer</p>
       {children}
     </div>
